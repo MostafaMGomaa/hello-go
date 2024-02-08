118d431a33bdab3fe6fbccca230de8c276b252a6
MostafaMGomaa <gomaamostafa26@gmail.com>
2024-02-08 05:08:41 +0200

    connect to mongodb

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var uri = "mongodb://localhost:27017/go_demo"
+var mongoClient *mongo.Client
 
 func main() {
 	r := gin.Default()
@@ -15,4 +21,13 @@ func main() {
 	})
 
 	r.Run()
+}
+
+func connectToMongoDB() error {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err == nil {
+		mongoClient = client
+		return nil
+	}
+	return err
 }
